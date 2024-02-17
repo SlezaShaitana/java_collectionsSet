c4b852dd7f878ee526859b515a868ef6722e63fa
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2024-02-17 21:47:25 +0300

    11.6 homework done

--- src/main/java/practice/EmailList.java
+++ src/main/java/practice/EmailList.java
@@ -1,19 +1,31 @@
 package practice;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
 public class EmailList {
+    TreeSet<String> treeSetEmail = new TreeSet<>();
 
     public void add(String email) {
         // TODO: валидный формат email добавляется, email это строка, она быть может любой
         // принять решение добавлять аргумент email или нет должен этот метод
+        String regex = "[A-z]+@[A-z]+[.][a-z]{2,4}";
+
+        if (email.matches(regex)) {
+            treeSetEmail.add(email.toLowerCase());
+        } else {
+            System.out.println(Main.WRONG_EMAIL_ANSWER);
+        }
+
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-        return new ArrayList<>();
+        for (String emails : treeSetEmail) {
+            System.out.println(emails);
+        }
+        return treeSetEmail.stream().toList();
     }
 
 }
--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,7 +1,12 @@
+package practice;
+
+import practice.EmailList;
+
 import java.util.Scanner;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
+    public static EmailList emailList = new EmailList();
     
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
@@ -24,6 +29,12 @@ public class Main {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
+            }
+            String[] splitInput = input.split(" ", 2);
+            switch (splitInput[0]) {
+                case "ADD" -> emailList.add(splitInput[1]);
+                case "LIST" -> emailList.getSortedEmails();
+                default -> System.out.println(WRONG_EMAIL_ANSWER);
             }
             
             //TODO: write code here
